e962b9a44eac5b47f3e0a489f6662547a68cb3dd
LeeHyeonHyeong <mhee12342@naver.com>
2015-05-31 03:42:01 +0900

    Update MyPageController.java

--- mysmarthompage/src/com/mysmarthomepage/join/MyPageController.java
+++ mysmarthompage/src/com/mysmarthomepage/join/MyPageController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/member/MyPage.do")
+@WebServlet("/member/mypage.do")
 public class MyPageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
