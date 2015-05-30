3a306d1916131ab1b626093adae701164698b17c
LeeHyeonHyeong <mhee12342@naver.com>
2015-05-31 03:52:58 +0900

    Update LoginServiceImpl.java

--- mysmarthompage/src/com/mysmarthomepage/login/LoginServiceImpl.java
+++ mysmarthompage/src/com/mysmarthomepage/login/LoginServiceImpl.java
@@ -1,11 +1,15 @@
-package com.mysmarthomepage.login;
+package com.smarthomepage.login;
 
-import com.mysmarthomepage.join.MemberServiceImpl;
-
-public class LoginServiceImpl{
+public class LoginServiceImpl implements LoginService {
 	private LoginServiceImpl() {}
 	private static LoginServiceImpl instance = new LoginServiceImpl();
 	public static LoginServiceImpl getInstance(){
 		return instance;
 	}
+	@Override
+	public String login(String id) {
+		
+		return LoginDAO.getInstance().login(id);
+	}
 }
+
